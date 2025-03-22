eaccff79d1e428ff47683d2763a2e63d703b19fa
gurrium <gurrium@hatena.ne.jp>
2025-03-22 13:10:33 +0900

    Initial Commit

---
+++ AIChang/AIChangApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChangApp.swift
+//  AIChang
+//
+//  Created by gurrium on 2025/03/22.
+//
+
+import SwiftUI
+
+@main
+struct AIChangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChangTests/AIChangTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChangTests.swift
+//  AIChangTests
+//
+//  Created by gurrium on 2025/03/22.
+//
+
+import Testing
+@testable import AIChang
+
+struct AIChangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
